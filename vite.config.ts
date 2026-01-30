9051e447a61580325687739878f30796024b0672
Augusto <aspastrana990@gmail.com>
2026-01-30 13:17:02 -0300

    Remove rollup config

--- vite.config.ts
+++ vite.config.ts
@@ -13,30 +13,5 @@ export default defineConfig({
     minify: "esbuild",
     cssCodeSplit: true,
     modulePreload: { polyfill: true },
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            // Separar React y bibliotecas grandes
-            if (id.includes("react-dom") || id.includes("react/")) return "react";
-            if (id.includes("@tanstack/react-query")) return "react-query";
-            if (id.includes("zustand")) return "zustand";
-            if (id.includes("framer-motion")) return "framer-motion";
-            if (id.includes("react-router")) return "react-router";
-            if (id.includes("react-calendar")) return "react-calendar";
-            if (id.includes("lucide-react")) return "lucide";
-            if (
-              id.includes("axios") ||
-              id.includes("clsx") ||
-              id.includes("tailwind-merge") ||
-              id.includes("zod")
-            ) {
-              return "vendor";
-            }
-            return "vendor-misc";
-          }
-        },
-      },
-    },
   },
 });
